7dc15b6f832fd73c35df05c33e21702072d3a96b
adc90 <aaronc81590@gmail.com>
2015-05-17 20:08:43 -0400

    Added in program list

--- env_linker.sh
+++ env_linker.sh
@@ -3,6 +3,8 @@
 #Set debugger
 set -x
 
+PackageManager="apt-get install"
+
 InternetUtilInstalled() {
     if hash curl 2>/dev/null; then
 	echo "curl is installed"
@@ -20,5 +22,23 @@ InstallPrograms() {
     done
 }
 
-InternetUtilInstalled
+CheckRoot(){
+    if [ "$(id -u)" != "0" ]; then
+	echo "Sorry, you are not root."
+	exit 1
+    else
+	echo "You're root"
+	exit 0
+    fi
+}
 
+CheckFile(){
+    #Check if the dots files are in the home
+    #directory and if they are move them
+    #gbd-doc
+    #bash-completion
+    #ln -sf
+}
+
+InternetUtilInstalled
+CheckRoot
