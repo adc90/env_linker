8cc5ade2c1fba914857aec43c8b9db344e0294f2
adc90 <aaronc815990@gmail.com>
2015-05-24 17:20:02 -0400

    Updated env_linker.sh

--- env_linker.sh
+++ env_linker.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+#====================================================================
+# Author: adc90
+# Description: Installs all the programs listed in program_list.json
+# and links all your dot files stored in '.dotfiles'
+#====================================================================
+
 #Set debugger
 #set -x
 
-PackageManager="apt-get install"
+
+#==========================================
+# Confinguration variables
+#==========================================
+PACKAGE_MANAGER="apt-get install"
+DOT_FILES=".dotfiles"
+
+
 
 function InternetUtilInstalled {
     if hash curl 2>/dev/null; then
         echo "curl is installed"
     elif hash wget 2>/dev/null; then
         echo "wget is installed"
-    else 
+    else
         echo "Please install curl or wget"
     fi
 }
 
 function InstallPrograms {
-    if [ -z "$1" ]; then
-        echo "No programs listed in programList.json"
-        exit 0
-    fi
     for x in $@; do
-        $PackageManager $x
+        $PACKAGE_MANAGER $x
     done
 }
 
@@ -41,20 +50,30 @@ function ParseList {
 }
 
 function LinkDotFiles {
-    directoryLock="true"
-    for x in $(ls -d .dotfiles/.* | grep "^\.."); do
-        if ln -s $x $HOME; then
-            echo $x
-        else
-            echo "Hi"
-        fi
-    done
+    if [ ! -d $DOT_FILES ]; then
+        echo "Please create a dotfiles directory for env_linker to mangage."
+    else
+        for x in $(ls -d ${DOT_FILES}/.* | grep "^\.."); do
+            #Test if the symbolic link has already been created
+            if ln -s $x $HOME; then
+                echo "Creating a symlink for: "$x
+            else
+                if [ ! -d "$HOME/.backup" ]; then
+                    mkdir "$HOME/.backup"
+                fi
+                echo "Moving old dotfile to .backup: "$x
+                mv $x "$HOME/.backup/"
+                echo "Creating a symlink for: "$x
+                ln -s $x $HOME
+            fi
+        done
+    fi
+
 }
 
 function env_linker {
-    #Check if the user is root otherwise exit
+    CheckRoot
     LinkDotFiles
-    CheckRoot 
     InstallPrograms $(ParseList)
 }
 
@@ -68,6 +87,12 @@ function CompareFiles {
     fi
 }
 
-#Main function
+function LinkTest {
+    for x in $(ls -d .dotfiles/.* | grep "^\.."); do
+        echo $x
+    done
+}
+
+#Run the main function
 env_linker
 
